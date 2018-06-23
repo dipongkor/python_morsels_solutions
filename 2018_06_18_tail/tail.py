5c267e8847fcc03b6fd1395d68bfae8d4e0cf8a6
Michael Anderson <mganderson87@gmail.com>
2018-06-22 21:23:56 -0400

    Add revised solution to tail

--- 2018_06_18_tail/tail.py
+++ 2018_06_18_tail/tail.py
@@ -1,16 +1,11 @@
-"""First attempt - 6/18/18"""
+"""Revised attempt - 6/22/18"""
+from collections import deque
 
 
 def tail(iterable, n):
     """Given an iterable, returns a list containing
     the last n elements in the iterable
     """
-    tail_elements = []
-    if n > 0:
-        for element in iterable:
-            tail_elements.append(element)
-            if len(tail_elements) > n:
-                # tail_elements is past max length, and we need
-                # to remove the first element
-                tail_elements = tail_elements[-n:]
-    return tail_elements
+    if n < 1:
+        return []
+    return list(deque(iterable, maxlen=n))
