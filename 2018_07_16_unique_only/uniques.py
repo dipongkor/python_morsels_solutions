9a69a3cd3cf16dfa64e1ede7142d2c72611014f0
Michael Anderson <mganderson87@gmail.com>
2018-07-17 20:08:49 -0400

    Initial solution to uniques_only exercise; accepts unhashable objects

---
+++ 2018_07_16_unique_only/uniques.py
@@ -0,0 +1,25 @@
+"""Initial solution to Uniques Only exercise
+
+07/08/2018
+"""
+from collections import Counter
+
+
+def uniques_only(iterable):
+    """Given an iterable, return an iterator containing all of
+    the elements in the initial iterable except for duplicates
+    """
+    try:
+        # This will raise TypeError if elements in iterable are unhashable
+        uniques = Counter(iterable)
+    except TypeError:
+        uniques = []
+        for item in iterable:
+            dupe_found = False
+            for element in uniques:
+                if item == element:
+                    dupe_found = True
+                    break
+            if not dupe_found:
+                uniques.append(item)
+    return (key for key in uniques)
