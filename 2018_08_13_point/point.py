c0d0a89df462bf0ce87f7b3a6a73fd9229a491fd
Michael Anderson <mganderson87@gmail.com>
2018-08-21 22:14:38 -0400

    Revise solution to point exercise for clarity

--- 2018_08_13_point/point.py
+++ 2018_08_13_point/point.py
@@ -1,4 +1,6 @@
-"""Initial solution to point exercise
+"""Revised solution to point exercise
+
+Removes _do_binary_op, which was needlessly confusing
 
 08/313/2018
 """
@@ -13,26 +15,25 @@ class Point(object):
     def __iter__(self):
         return (attr for attr in (self.x, self.y, self.z))
 
-    def __str__(self):
+    def __repr__(self):
         return "Point({}, {}, {})".format(self.x, self.y, self.z)
 
-    def __repr__(self):
-        return self.__str__()
-
     def __eq__(self, other):
-        return (self.x, self.y, self.z) == (other.x, other.y, other.z)
-
-    def _do_binary_op(self, other, op):
-        return Point(*[op(a, b) for a, b in zip(self, other)])
+        x1, y1, z1 = self
+        x2, y2, z2 = other
+        return (x1, y1, z1) == (x2, y2, z2)
 
     def __add__(self, other):
-        return self._do_binary_op(other, op=lambda x, y: x + y)
+        x1, y1, z1 = self
+        x2, y2, z2 = other
+        return Point(x1+x2, y1+y2, z1+z2)
 
     def __sub__(self, other):
-        return self._do_binary_op(other, op=lambda x, y: x - y)
+        x1, y1, z1 = self
+        x2, y2, z2 = other
+        return Point(x1-x2, y1-y2, z1-z2)
 
-    def __mul__(self, factor):
-        other = Point(factor, factor, factor)
-        return self._do_binary_op(other, op=lambda x, y: x * y)
+    def __mul__(self, scalar):
+        return Point(*(scalar * coord for coord in self))
 
     __rmul__ = __mul__  # 2 * Point(x,y,z) == Point(x,y,z) * 2
