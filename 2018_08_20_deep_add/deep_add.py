43a9c91d35c2a25f6d9037e7e41e3f29c79dad0e
Michael Anderson <mganderson87@gmail.com>
2018-08-27 20:39:00 -0400

    Fix confusingly named var in deep_add - it's a stack

--- 2018_08_20_deep_add/deep_add.py
+++ 2018_08_20_deep_add/deep_add.py
@@ -5,13 +5,13 @@
 
 
 def deep_add(nested_list, start=0):
-    q = [nested_list]
-    while q:
-        element = q.pop()
+    stack = [nested_list]
+    while stack:
+        element = stack.pop()
         try:
             # If the element is iterable, add its children to queue
             for child in element:
-                q.append(child)
+                stack.append(child)
         except TypeError:
             # It's not iterable -- add it to the sum
             start += element
